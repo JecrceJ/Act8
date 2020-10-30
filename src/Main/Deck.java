cdf62bd6bb60bde8cac413bc079574f19e806228
JecrceJ <al02813049@tecmilenio.mx>
2020-10-29 22:58:47 -0600

    Manejo de excepciones

--- src/Main/Deck.java
+++ src/Main/Deck.java
@@ -9,22 +9,39 @@ public class Deck extends Card{
         System.out.println("**********Metodo shuffle*******" +
                 "\n" +     "Se mezclo el deck \n");
     }
-    public void head(ArrayList<String> Cards){
-        System.out.println("**********Metodo head*******\n" +Cards.get(1));
-        Cards.remove(1);
-        System.out.println("Quedan " + Cards.size() + "\n");
+    public void head(ArrayList<String> Cards) throws IndexOutOfBoundsException{
+        try {
+            System.out.println("**********Metodo head*******\n" + Cards.get(1));
+            Cards.remove(1);
+            System.out.println("Quedan " + Cards.size() + "\n");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("NO hay mas cartas en el deck");
+            System.exit(0);
+        }
     }
-    public void pick(ArrayList<String> Cards){
-        Collections.shuffle(Cards);
-        System.out.println("**********Metodo pick*******\n" + Cards.get(5));
-        Cards.remove(5);
-        System.out.println("Quedan " + Cards.size() +" \n");
+    public void pick(ArrayList<String> Cards) throws IndexOutOfBoundsException{
+        try {
+            Collections.shuffle(Cards);
+            System.out.println("**********Metodo pick*******\n" + Cards.get(5));
+            Cards.remove(5);
+            System.out.println("Quedan " + Cards.size() + " \n");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("NO hay mas cartas en el deck");
+            System.exit(0);
+        }
     }
-    public void hand(ArrayList<String> Cards){
-        System.out.println("**********Metodo shuffle*******");
-        for (int i=0; i<5; i++) {
-            System.out.println(Cards.get(i));
-            Cards.remove(i);
+    public void hand(ArrayList<String> Cards) throws IndexOutOfBoundsException{
+        try {
+            System.out.println("**********Metodo shuffle*******");
+            for (int i = 0; i < 5; i++) {
+                System.out.println(Cards.get(i));
+                Cards.remove(i);
+            }
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("***************************************" +
+                             "\n******No hay mas cartas en el deck*****" +
+                             "\n***************************************");
+            System.exit(0);
         }
         System.out.println("Quedan " + Cards.size() +"\n");
     }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,7 @@
 package Main;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {                                                      //Creacion de la clase principal
@@ -9,31 +10,38 @@ public class Main {                                                      //Creac
         int sel;
         Deck deck = new Deck();                                         //Iniciacion de las clases
         Card card = new Card();
-        Cards=card.collection();
+        Cards = card.collection();
         deck.shufle(Cards);
         System.out.println("Bienvenido a Poker! \n");
         do {
-            sel=showMenu();
-        switch (sel) {
-            case 1 -> deck.shufle(Cards);
-            case 2 -> deck.head(Cards);
-            case 3 -> deck.pick(Cards);
-            case 4 -> deck.hand(Cards);
-        }
-        } while (sel!=0);
+            sel = showMenu();
+            switch (sel) {
+                case 1 -> deck.shufle(Cards);
+                case 2 -> deck.head(Cards);
+                case 3 -> deck.pick(Cards);
+                case 4 -> deck.hand(Cards);
+            }
+        } while (sel != 0);
     }
-    public static int showMenu() {
-        Scanner sc = new Scanner(System.in);
-        int a;
-        System.out.println("Selecciona una opcion:\n" +
-                "1 Mezclar deck\n" +
-                "2 Sacar una carta\n" +
-                "3 Carta al azar\n" +
-                "4 Generar una mano de 5 cartas\n" +
-                "0 Salir");
-        a = sc.nextInt();
-        if (a >= 5) {
-            System.out.println("Opcion no valida"); }
+
+    public static int showMenu() throws InputMismatchException{
+        try {
+            Scanner sc = new Scanner(System.in);
+            int a;
+            System.out.println("Selecciona una opcion:\n" +
+                    "1 Mezclar deck\n" +
+                    "2 Sacar una carta\n" +
+                    "3 Carta al azar\n" +
+                    "4 Generar una mano de 5 cartas\n" +
+                    "0 Salir");
+
+            a = sc.nextInt();
+            if (a>4)
+                System.out.println("Ingrese una opcion del menu");
             return a;
+        } catch (InputMismatchException e) {
+            System.out.println("Es necesario que digite una opcion del menu");
         }
+        return 9;
     }
+}
